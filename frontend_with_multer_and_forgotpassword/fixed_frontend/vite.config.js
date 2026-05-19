f8742621419a96c38df705d0c461ca83a90e6bf3
Rajeev Kumar <kumarrajeev6785@gmail.com>
2026-05-19 10:37:23 +0530

    Update vite.config.js

--- frontend_with_multer_and_forgotpassword/fixed_frontend/vite.config.js
+++ frontend_with_multer_and_forgotpassword/fixed_frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://himachal-agrorent.onrender.com/',
         changeOrigin: true,
       },
     },
